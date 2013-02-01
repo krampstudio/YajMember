473f0ba7b574ccdb7b385c86cc74796adedb000a
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2013-02-01 22:28:24 +0100

    update properties to manage mongo conf

--- src/main/java/org/yajug/users/persistence/MongoConnector.java
+++ src/main/java/org/yajug/users/persistence/MongoConnector.java
@@ -2,25 +2,46 @@ package org.yajug.users.persistence;
 
 import java.net.UnknownHostException;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.inject.name.Named;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 
 @Singleton
 public class MongoConnector {
-
+	
 	private static MongoClient mongoClient;
 	private DB database;
 	
-	public MongoConnector(){
+	@Inject 
+	public MongoConnector(
+			@Named("db.host") String host,
+			@Named("db.port") int port,
+			@Named("db.name") String name
+		){
+		
+		//set default values
+		if(StringUtils.isBlank(host)){
+			host = "localhost";
+		}
+		if(port <= 0){
+			port = 27017;
+		}
+		if(StringUtils.isBlank(name)){
+			host = "yajmember";
+		}
+		
 		if(mongoClient == null){
 			try {
-				mongoClient = new MongoClient("localhost", 27017);
+				mongoClient = new MongoClient(host, port);
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 			}
 		}
-		database = mongoClient.getDB("yajmember");
+		database = mongoClient.getDB(name);
 	}
 	
 	public DB getDatabase() {
