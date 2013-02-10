005235b88cdc76ca4efbe4bb642e8c3f08c730a9
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2013-02-10 23:12:38 +0100

    try require.js optimisation

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,30 +1,17 @@
-//RequireJs configuration: 
-requirejs.config({
-	urlArgs: 'bust=' + (new Date()).getTime(),	//only for dev  : no-cache for the laoded scripts 
-	paths: {
-		'jquery' 		: 'lib/jquery/jquery',
-		'noty-base'		: 'lib/noty/js/noty',
-		'noty'			: 'lib/noty/js/noty/jquery.noty',
-		'notify'		: 'helpers/notify',
-		'uri'			: 'helpers/uri'
-	},
-	//dependencies
-	shim: {
-		'uri'				: ['jquery'],
-	    'noty' 				: ['jquery'],
-	    'notify'			: ['noty']
-	}
-});
-
-//The main entry point
-requirejs(['jquery','notify', 'uri'], function($, notify, uri){
-
-	$(function(){
+//load the configuration
+require(['config/login'], function(){
+	
+	//login page entry point
+	requirejs(['jquery','notify', 'uri'], function($, notify, uri){
 		
-		//if the window contains an error param in the query we display an error message
-		var params = uri.getQueryParams(window.location.href);
-		if(params.error){
-			notify('error', params.error);
-		}
+		//on page load
+		$(function(){
+			
+			//if the window contains an error param in the query we display an error message
+			var params = uri.getQueryParams(window.location.href);
+			if(params.error){
+				notify('error', params.error);
+			}
+		});
 	});
 });
