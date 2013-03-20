9457eb6ca057e26dcd8a19cc4251dddec1f40084
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2013-03-20 14:07:37 +0100

    enable to use the sendfile plugin on multiple form

--- src/main/webapp/js/ui/filesender.js
+++ src/main/webapp/js/ui/filesender.js
@@ -10,54 +10,96 @@ define( ['jquery'], function($){
 	 */
 	var FileSender = {
 	
+		/**
+		 * The default options
+		 */
 		_opts : {
 			url : '',
-			frame : '__postFrame',
+			frame : '__postFrame_',
 			loaded : function(data){}
 		},
-			
+		
+		/**
+		 * Initialize the file sending
+		 *  @param {Object} options - the sending options
+		 *  @param {String} [options.url] - the url where the form will send the file, if not set we get the form.action attr
+		 *  @param {String} [options.frame] - a name for the frame create in background
+		 *  @param {FileLoadedCallback} [options.loaded] - executed once received the server response
+		 */
 		_init : function(options){
 			var self = FileSender,
 	            opts = $.extend(true, {}, self._opts, options),
-	            $form = this;
+	            $form = this,
+	            id = opts.frame;
 			
 			if(!$form || !$form.is('form')){
-				$.error('The function requires a jQuery element that matches a form');
+				$.error('This plugin can only be called on a FORM element');
+			}
+			if(!$form.attr('action') || !opts.url || opts.url.trim().length == 0){
+				$.error('An url is required in the options or at least an action ');
+			}
+			if($form.find("input[type='file']").length == 0){
+				$.error('This plugin is used to post files, your form should include an input element of type file.');
 			}
 			
+			//the iframe identifier is composed by opts.frame + (form.id or form.name or timestamp)
+			//the timestamp is the worth because if the response goes wrong we will not be able to remove it
+			id += ($form.attr('id') ?  $form.attr('id') : ($form.attr('name') ?  $form.attr('name') : (new Date()).getTime()));
+			
 			//clean up if already exists
-			$('#' + opts.frame, $form).remove();
+			$('#' + id).remove();
 			
 			//we create the hidden frame as the action of the upload form (to prevent page reload)
 			var $postFrame = $("<iframe />");
 			$postFrame.attr({
-				'name': opts.frame,
-				'id' : opts.frame
+				'name': id,
+				'id' : id
 			})
-			.css('display', 'none')
-			.load(function(){
-				//we get the response in the frame
-				var result = $.parseJSON($(this).contents().text());
-				if(typeof opts.loaded === 'function'){
-					 opts.loaded(result);
-				}
-			});
+			.css('display', 'none');
+			
 			
 			//we update the form attributes according to the frame
 			$form.attr({
 					'action'	: opts.url,
 					'method'	: 'POST',
 					'enctype'	: 'multipart/form-data',
-					'encoding'	: 'multipart/form-data',
-					'target'	: opts.frame
+					'encoding'	: 'utf8',
+					'target'	: id
 				})
-				.append($postFrame)
-				.submit();
-		}
+				.append($postFrame);
 			
+			$('#' + opts.frame, $form).on('load', function(e){
+				$(this).off('load');
+				
+				//we get the response in the frame
+				var result = $.parseJSON($(this).contents().text());
+				
+				if(typeof opts.loaded === 'function'){
+					 opts.loaded(result);
+				}
+				
+				$(this).remove();
+			});
+				
+			$form.submit();
+		}
 	};
 	
+	/**
+	 * Reference the plugin to the jQuery context 
+	 * to be able to call as $('#aForm').sendfile({'url' : '/api/postfile'});
+	 *  @param {Object} options - the sending options
+	 *  @param {String} options.url - the url where the form will send the file
+	 *  @param {String} [options.frame] - a name for the frame create in background
+	 *  @param {FileLoadedCallback} [options.loaded] - executed once received the server response
+	 */
 	$.fn.sendfile = function(options){
 		return FileSender._init.call(this, options);
 	};
+	
+	/**
+	 * Callback function to receive the server response of posted file
+	 * @callback FileLoadedCallback
+	 * @param {Object} data - the evaluated JSON response sent by the server
+	 */
 });
