49c69522f6cd2980ff8211c48e20af679fe1c209
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2013-04-08 14:05:01 +0200

    start to implement participant chart

--- src/main/webapp/js/report/stats.js
+++ src/main/webapp/js/report/stats.js
@@ -1,47 +1,70 @@
-define(['jquery', 'chart'], function($){
+define(['jquery', 'controller/event', 'eventbus', 'chart'], function($, EventController, EventBus){
 	
 	return {
 		setUp : function(){
-			var chart1 = new Chart($('#stat-members').get(0).getContext("2d"));
-			var chart2 = new Chart($('#stat-events').get(0).getContext("2d"));
-			
-			chart1.Bar({
-				labels : ["January","February","March","April","May","June","July"],
-				datasets : [
-					{
-						fillColor : "rgba(220,220,220,0.5)",
-						strokeColor : "rgba(220,220,220,1)",
-						data : [65,59,90,81,56,55,40]
-					},
-					{
-						fillColor : "rgba(151,187,205,0.5)",
-						strokeColor : "rgba(151,187,205,1)",
-						data : [28,48,40,19,96,27,100]
+			EventBus.subscribe('chart.participant.update', function(event, year){
+				
+				EventController.getAll(year, function(events){
+					
+					var chart1; 
+					var $canvas = $('#stat-participants');
+					var labels = [];
+					var participants = [];
+					var registrants = [];
+					
+					for(var i in events){
+						labels.push(events[i].date.replace("2012-", '')+ " ");
+						if(events[i].participants && $.isArray(events[i].participants)){
+							participants.push(events[i].participants.length);
+						} else {
+							participants.push(0);
+						}
+						//participants.push(10 * i);
+						if(events[i].registrants && $.isArray(events[i].registrants)){
+							registrants.push(events[i].registrants.length);
+						} else {
+							registrants.push(0);
+						}
+						//registrants.push(15 * i);
 					}
-				]
-			}, {
-				scaleShowLabels : true
+					var width = labels.length * 75
+					$canvas.attr('width', (width < 150) ? 150 : width);
+					$canvas.attr('height', '300')
+					
+					chart1 = new Chart($canvas.get(0).getContext("2d"));
+					chart1.Bar({
+						labels : labels,
+						datasets : [
+							{
+								fillColor : "rgba(151,187,205,0.5)",
+								strokeColor : "rgba(151,187,205,1)",
+								data : registrants
+							},
+							{
+								fillColor : "rgba(231,113,36,0.5)",
+								strokeColor : "rgba(231,113,36,1)",
+								data : participants
+							}
+						]
+					}, {
+						scaleShowLabels : true
+					});
+				});
 			});
 			
-			chart2.Line({
-				labels : ["January","February","March","April","May","June","July"],
-				datasets : [
-					{
-						fillColor : "rgba(220,220,220,0.5)",
-						strokeColor : "rgba(220,220,220,1)",
-						pointColor : "rgba(220,220,220,1)",
-						pointStrokeColor : "#fff",
-						data : [65,59,90,81,56,55,40]
-					},
-					{
-						fillColor : "rgba(151,187,205,0.5)",
-						strokeColor : "rgba(151,187,205,1)",
-						pointColor : "rgba(151,187,205,1)",
-						pointStrokeColor : "#fff",
-						data : [28,48,40,19,96,27,100]
+			EventController.getYears(function(years){
+				if(years.length > 0){
+					for(var i in years){
+						$('#stat-participants-year')
+							.append($.tmpl("<option value='${item}'>${item}</option>", {item : years[i]}));
 					}
-				]
-			}, {});
+					$('#stat-participants-year').on('change', function(){
+						EventBus.publish('chart.participant.update', [$(this).val()]);
+					});
+					EventBus.publish('chart.participant.update', [$('#stat-participants-year').val()]);
+				}
+				//TODO throw error else
+			});
 		}
 	};
 	
